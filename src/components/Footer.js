a8190000347e016ca928b6fececc162e379fa1a6
ndongaloris <ndongajared@gmail.com>
2024-10-17 15:19:40 +0100

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,41 @@ import "./footer.css"
 export const Footer = () => {
   return (
     <div className="footer">
-      <p>@ copyright 2024 | Loris Jared Ndonga</p>
-      <nav>
+      <h4>myMateTask</h4>
+    <section className="footer-nav">
+
+      <div>
+        <h5>Home</h5>
+      </div>
+
+      <div>
+        <h5>Goals</h5>
         <ul>
-          <li><a href="">Instagram</a></li>
-          <li><a href="">Facebook</a></li>
-          <li><a href="">LinkedIn</a></li>
+            <li>Spiritual</li>
+            <li>Social</li>
+            <li>Physical</li>
+            <li>Intellectual</li>
         </ul>
-      </nav>
+      </div>
+
+      <div>
+        <h5>Profile</h5>
+        <ul>
+            <li>User settings</li>
+            <li>statistics</li>
+        </ul>
+      </div>
+
+      <div>
+        <h5>Resources</h5>
+        <ul>
+            <li>Articles</li>
+            <li>videos</li>
+            <li>motivational quotes</li>
+        </ul>
+      </div>
+    </section>
+      <p>&copy; copyright 2024 | Loris Jared Ndonga</p>
     </div>
   )
 }
